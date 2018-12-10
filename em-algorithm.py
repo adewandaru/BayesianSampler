bde54cab1dcdbef7aad53523d05cc76bf9552a6d
Agung Dewandaru <dewandaru@gmail.com>
2018-12-10 09:38:37 +0700

    em algorithm update

--- em-algorithm.py
+++ em-algorithm.py
@@ -48,16 +48,33 @@ plt.hist(t2, bins=100)
 mu = [0, 10]
 sigma = [1, 1]
 '''
-p1 = P(x|c1) = ...
-p2 = P(x|c2) = ...
+Expectation step: calculate "responsibility" of each cluster to each datapoints.
+a.k.a which 
+p_c1 = P(c1|x) = ...
+p_c2 = P(c2|x) = ...
 '''
-p1 = 
-p2 = 
+p_c1 = 
+p_c2 = 
 
 '''
+Maximization of Likelihood
 adjust mu and sigma 
 '''
 mu = 
 sigma = 
 
+def p_x_given_c(x, mu, sigma):
+    ''' just a typical normal pdf '''
+    return np.exp( -(x-mu)**2 / (2*sigma**2) ) / np.sqrt( 2*math.pi*sigma**2 )   
 
+def p_c_given_x(mu, sigma, x, prior_c):
+    ''' 
+    calculate responisibility p(c|x) for each datapoint. where c is the cluster.
+                   p(x|c) p(c)              p(x|c) p(c)                     p(x|c) p(c)
+    p(c|x) =  --------------------- = ------------------------- =  -----------------------------
+                      p(x)                  sum(c) p(x,c)             p(x|c)p(c) + p(x|c')p(c')  
+    '''
+    pxc = P_x_given_c(x, mu, sigma)
+    return pxc*prior 
+     
+    
